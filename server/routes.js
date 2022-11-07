04c27cdb28f667791460d53d91047b753ef5c40f
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-11-07 14:50:06 -0600

    118 Servicios REST

--- server/routes.js
+++ server/routes.js
@@ -5,40 +5,62 @@ var router = express.Router();
 
 const mensajes = [
 
-  {
-    _id: 'XXX',
-    user: 'spiderman',
-    mensaje: 'Hola Mundo'
-  }
+    {
+        _id: 'XXX',
+        user: 'spiderman',
+        mensaje: 'Hola Mundo'
+    }
 
 ];
 
 
 // Get mensajes
-router.get('/', function (req, res) {
-  // res.json('Obteniendo mensajes');
-  res.json( mensajes );
+router.get('/', function(req, res) {
+    // res.json('Obteniendo mensajes');
+    res.json(mensajes);
 });
 
 
 // Post mensaje
-router.post('/', function (req, res) {
-  
-  const mensaje = {
-    mensaje: req.body.mensaje,
-    user: req.body.user
-  };
+router.post('/', function(req, res) {
 
-  mensajes.push( mensaje );
+    const mensaje = {
+        mensaje: req.body.mensaje,
+        user: req.body.user
+    };
 
-  console.log(mensajes);
+    mensajes.push(mensaje);
+
+    console.log(mensajes);
 
 
-  res.json({
-    ok: true,
-    mensaje
-  });
+    res.json({
+        ok: true,
+        mensaje
+    });
 });
+
+//almacenar suscripcion
+router.post('/subscribe', (req, res) => {
+
+    res.json('subscribe');
+
+});
+
+router.get('/key', (req, res) => {
+
+
+    res.send('key');
+
+});
+
+//enviar una notificacion a las perosnas
+router.post('/push', (req, res) => {
+
+    res.json(post);
+
+});
+
 
 
 
