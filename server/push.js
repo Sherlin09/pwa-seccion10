bb2b62306a14daae9ae8796c85788201038f11aa
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-11-09 15:56:16 -0600

    123 guardar suscripciones

--- server/push.js
+++ server/push.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const urlsafeBase64 = require('urlsafe-base64');
 const vapid = require('./vapid.json');
 
@@ -5,10 +6,12 @@ module.exports.getKey = () => {
     return urlsafeBase64.decode(vapid.publicKey);
 };
 
-const suscripciones = [];
+const suscripciones = require('./subs-db.json');
 
 module.exports.addSubscription = (suscripcion) => {
 
     suscripciones.push(suscripcion);
 
+
+    fs.writeFileSync(`${ __dirname }/subs-db.json`, JSON.stringify(suscripciones));
 };
